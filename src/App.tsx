0b97dcad2f9fd9a3b3c3fae1e4f1b1daa14b6aaf
Salah Ghomrawi <ghoumrawi2020@gmail.com>
2020-09-28 02:50:05 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,125 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import React, { useState } from "react";
+import {
+  IonApp,
+  IonHeader,
+  IonContent,
+  IonToolbar,
+  IonTitle,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButton,
+  IonIcon,
+  IonCard,
+  IonCardContent,
+} from "@ionic/react";
+
+import { calculatorOutline, refreshOutline } from "ionicons/icons";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const [height, setHeight] = useState<any>();
+  const [weight, setWeight] = useState<any>();
+  const [bmi, setBmi] = useState<any>();
+
+  const calculateBmi = () => {
+    if (!weight || !height) {
+      return;
+    }
+
+    const newBmi = weight / (height * height);
+    setBmi(newBmi);
+  };
+
+  const resetValues = () => {
+    setHeight("");
+    setWeight("");
+    setBmi("");
+  };
+
+  return (
+    <IonApp>
+      <IonHeader>
+        <IonToolbar color="secondary">
+          <IonTitle>BMI Calculator</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating">Your Height</IonLabel>
+                <IonInput
+                  type="number"
+                  value={height}
+                  onIonChange={(e) => setHeight(+e.detail.value!)}
+                  clearInput
+                ></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating">Your Weight</IonLabel>
+                <IonInput
+                  type="number"
+                  value={weight}
+                  onIonChange={(e) => setWeight(+e.detail.value!)}
+                  clearInput
+                ></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow className="ion-text-center">
+            <IonCol>
+              <IonButton onClick={calculateBmi}>
+                <IonIcon slot="start" icon={calculatorOutline} />
+                Calculate
+              </IonButton>
+            </IonCol>
+            <IonCol>
+              <IonButton onClick={resetValues}>
+                <IonIcon slot="start" icon={refreshOutline} />
+                Reset
+              </IonButton>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              {bmi && (
+                <IonCard>
+                  <IonCardContent>{bmi}</IonCardContent>
+                </IonCard>
+              )}
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonApp>
+  );
+};
 
 export default App;
